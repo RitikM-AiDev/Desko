76acaa45435a75d8eb6ac9687998bb16c2a18499
Ritik M <ritikmohanbabu@gmail.com>
2026-06-23 12:32:58 +0530

    Update Web.py

--- api/Web.py
+++ api/Web.py
@@ -17,13 +17,13 @@ from langchain_community.utilities import GoogleSerperAPIWrapper
 from .Tools import browser_tool
 load_dotenv(override=True)
 llm_classifier = ChatOpenAI(
-    model="gemini-2.5-flash",
+    model="gemini-3-flash-preview",
     openai_api_key=os.getenv("GEMINI_API_KEY"),
     openai_api_base=os.getenv("GEMINI_BASE_URL"),
 )
 
 llm_agent = ChatOpenAI(
-    model="gemini-2.5-flash",
+    model="gemini-3-flash-preview",
     openai_api_key=os.getenv("GEMINI_API_KEY"),
     openai_api_base=os.getenv("GEMINI_BASE_URL"),
 )
